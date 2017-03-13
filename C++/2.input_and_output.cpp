8c8c51e5ba140cf6a1b7134bc15b0aeb0755bc50
Ajith <ajithaj.418aj@gmail.com>
2017-03-13 16:15:51 +0530

    2.input_and _output program solution

---
+++ C++/2.input_and_output.cpp
@@ -0,0 +1,8 @@
+#include<cstdio>
+using namespace std;
+int main(){
+    int a,b,c;  //declaring the integers
+    scanf("%d %d %d",&a,&b,&c); //reading the integers
+    printf("%d",a+b+c);  //adding and printing them
+    return 0;
+}
